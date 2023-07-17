56aa4ae1d4c3c0b3f01ec6f8548aafcbee1267e4
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2023-07-17 16:02:04 +0530

    create frontend design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './Pages/Home';
+import Main from './Pages/Main';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route exact path="/main">
+                  <Main />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        
+    </>
   );
 }
 
