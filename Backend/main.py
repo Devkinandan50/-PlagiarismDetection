f0357f772b34b23937d5122cfdfc0b4bc5483f38
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2023-07-18 13:03:06 +0530

    save some changes

--- Backend/main.py
+++ Backend/main.py
@@ -3,22 +3,12 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
-def upload_files():
-    # Check if the request contains two PDF files
-    if 'file1' not in request.files or 'file2' not in request.files:
-        return jsonify({'message': 'Two PDF files are required.'}), 400
-    
-    file1 = request.files['file1']
-    file2 = request.files['file2']
-
-    # Check if the file extensions are PDF
-    if not file1.filename.endswith('.pdf') or not file2.filename.endswith('.pdf'):
-        return jsonify({'message': 'Both files should be in PDF format.'}), 400
-
-    # Process the files (you can add your own logic here)
-
-    # Return a success response
-    return jsonify({'message': 'Files uploaded successfully.'}), 200
+def concatenate_strings():
+    data = request.get_json()
+    string1 = data['string1']
+    string2 = data['string2']
+    result = string1 + string2
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useContext, useEffect, useState} from 'react'
 
 const Main = () => { 
+    const [pdfFile1, setPdfFile1] = useState(null);
+    const [pdfFile2, setPdfFile2] = useState(null);
+    const [validationError, setValidationError] = useState('');
+
+    const handleFile1Change = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type === 'application/pdf') {
+          setPdfFile1(file);
+          setValidationError('');
+        } else {
+          setPdfFile1(null);
+          setValidationError('Please select a PDF file for File 1.');
+        }
+      };
+    
+      const handleFile2Change = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type === 'application/pdf') {
+          setPdfFile2(file);
+          setValidationError('');
+        } else {
+          setPdfFile2(null);
+          setValidationError('Please select a PDF file for File 2.');
+        }
+      };
+    
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        if (pdfFile1 && pdfFile2) {
+          // Perform further processing with the PDF files
+          console.log('File 1:', pdfFile1);
+          console.log('File 2:', pdfFile2);
+          alert("pdf gone")
+        } else {
+          setValidationError('Please select both PDF files.');
+        }
+      };
+
+
     return (
         <div>
-            This is Main page new branch
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="file1">File 1:</label>
+          <input type="file" id="file1" accept="application/pdf" onChange={handleFile1Change} />
         </div>
+        <div>
+          <label htmlFor="file2">File 2:</label>
+          <input type="file" id="file2" accept="application/pdf" onChange={handleFile2Change} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {validationError && <p>{validationError}</p>}
+    </div>
     )
 }
 
