d9fdf0b9ab346cf68299733313fa95a8e7991f96
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2023-07-19 13:16:04 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,19 +1,3 @@
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
-# @app.route('/upload', methods=['POST'])
-# def concatenate_strings():
-#     data = request.get_json()
-#     string1 = data['string1']
-#     string2 = data['string2']
-#     result = string1 + string2
-#     return jsonify({'result': result})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 import PyPDF2
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
